ca89063b24bf2e91722cbf0251593ad2c145a586
Patrick-G-Oliver <patrickgriffitholiver@gmail.com>
2020-05-22 13:07:32 -0400

    AddArticle-PatrickOliver

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import styled from "styled-components";
+
+
+
+export default NavBar;
+
+// sign up, log in, log out(?), home(i.e. landing page?), article board(?),
+// add / delete article(?)

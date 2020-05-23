c5024aa3e77c9400cbd22af689be7a156426a9e6
Patrick-G-Oliver <patrickgriffitholiver@gmail.com>
2020-05-22 21:07:51 -0400

    trying to update local with remote

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import styled from "styled-components";
-
-
-
-export default NavBar;
-
-// sign up, log in, log out(?), home(i.e. landing page?), article board(?),
-// add / delete article(?)
-=======
 import React from "react";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
@@ -77,4 +65,3 @@ const NavBar = () => {
 };
 
 export default NavBar;
->>>>>>> master
